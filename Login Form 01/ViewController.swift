5b3907ea39d97f80c91afd892a0c67678b1d0aa1
R. Kukuh <rkukuh@gmail.com>
2018-03-31 15:50:30 +0700

    Upgrade to Xcode 9.3 recommendation setting

--- Login Form 01/ViewController.swift
+++ Login Form 01/ViewController.swift
@@ -9,17 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var pinField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+        let email : String = emailField.text!
+        let pin : String = pinField.text!
+        
+        if email.isEmpty || pin.isEmpty {
+            
+            print("Email / PIN is required !")
+            
+            return;
+        }
+        
+        if (email == "admin@admin.com") && (pin == "1234") {
+            
+            let alert = UIAlertController(title: "Sukses",
+                                          message: "Welcome back, Super Admin!",
+                                          preferredStyle: .alert)
+            
+            let OkAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            
+            alert.addAction(OkAction)
+            
+            present(alert, animated: true, completion: nil)
+            
+        } else {
+            
+            let alert = UIAlertController(title: "Gagal",
+                                          message: "Your Email / PIN Incorrect!",
+                                          preferredStyle: .alert)
+            
+            let OkAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            
+            alert.addAction(OkAction)
+            
+            present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
 }
-
